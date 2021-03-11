c69ff2f1d6ed491a1a21805d7f63498761f07267
Dan Smith <dan@sxcreators.com>
2021-03-11 00:51:45 +0000

    Style buttons

--- index.php
+++ index.php
@@ -11,13 +11,13 @@
     <p class="lead">Choose an option below to get started.</p>
   </div>
 <div class="row align-items-start">
-<div class="col"><h5 class="homebtns">Analytics</h5></div>
+<div class="col"><h5 class="homebtns"><img src="assets/images/graph-up.svg" style="width: 50%; margin-top: -5px">Analytics</h5></div>
 
-<div class="col"><h5 class="homebtns">Access Points</h5></div>
+<div class="col"><h5 class="homebtns"><img src="assets/images/wifi.svg" style="width: 66%; margin-top: -20px">Access Points</h5></div>
 
-<div class="col"><h5 class="homebtns">Blocked Devices</h5></div>
+<div class="col"><h5 class="homebtns"><img src="assets/images/slash-circle.svg" style="width: 60%; margin-top: -10px">Blocked Devices</h5></div>
 
-<div class="col"><h5 class="homebtns">Switches</h5></div>
+<div class="col"><h5 class="homebtns"><img src="assets/images/switch-rack.svg" style="width: 60%; margin-top: -10px">Switches</h5></div>
 
 </div>
 </main><!-- /.container -->
