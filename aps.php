2f1696c59ed28b73292697cc10be4fbe50a640e2
Dan Smith <dan@sxcreators.com>
2021-03-14 02:32:12 +0000

    Add API and test listing device and clients

--- aps.php
+++ aps.php
@@ -1,5 +1,6 @@
 <?php include "core.html"; ?>
-
+<?php include "include/unifiapi/src/config.php"; ?>
+<?php include "include/unifiapi/src/Client.php"; ?>
 <!doctype html>
 <html lang="en">
 
@@ -9,7 +10,23 @@
   <div class="starter-template text-center py-5 px-3">
     <h1>Access Points</h1>
     <p class="lead">Information about your access points.</p>
->
+
+<?php
+$unifi_connection = new UniFi_API\Client($controlleruser, $controllerpassword, $controllerurl, $site_id, $controllerversion);
+$set_debug_mode   = $unifi_connection->set_debug($debug);
+$loginresults     = $unifi_connection->login();
+$aps_array        = $unifi_connection->list_devices();
+
+/**
+ * output the results in HTML format
+ */
+header('Content-Type: text/html; charset=utf-8');
+foreach ($aps_array as $ap) {
+    if ($ap->type === 'uap') {
+        echo '<b>AP name:</b>' . $ap->name . ' <b>model:</b>' . $ap->model . ' <b># connected clients:</b>' . $ap->num_sta . '<br>';
+    }
+}
+?>
   </div>
 </main><!-- /.container -->
 
